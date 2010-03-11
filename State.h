6638dde81302fe6f4f4561ad59435c7905832e54
Petr Smejkal <xmejkal@gmail.com>
2010-03-11 12:08:53 +0100

    State.h empty

--- State.h
+++ State.h
@@ -1,24 +1,12 @@
 /*
  * State.h
  *
- *  Created on: Mar 4, 2010
+ *  Created on: Mar 11, 2010
  *      Author: smejkalp
  */
 
-#include <vector>
+#ifndef STATE_H_
+#define STATE_H_
 
-#ifndef STATE_H_24563457568768
-#define STATE_H_24563457568768
 
-using namespace std;
-
-typedef vector<  vector<int> > matrix;
-class State {
-public:
-
-	matrix incidence;
-	State(matrix incidence);
-	virtual ~State();
-};
-
-#endif /* STATE_H_24563457568768 */
+#endif /* STATE_H_ */
