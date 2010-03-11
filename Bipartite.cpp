896b21aff39a31c12b90956e97c0593d8680007b
Petr Smejkal <xmejkal@gmail.com>
2010-03-11 12:12:24 +0100

    .

--- Bipartite.cpp
+++ Bipartite.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Bipartite.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
