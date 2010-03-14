62c44de40d23cc94cd7b73b5d3cbcc3fe3944536
Petr Smejkal <xmejkal@gmail.com>
2010-03-14 23:56:35 +0100

    loading v main

--- Bipartite.cpp
+++ Bipartite.cpp
@@ -27,7 +27,28 @@ void writeSolution(){
 int main() {
 
 	GraphReader reader;
-	State *state1 = reader.getFirstStateFromFile("zk.txt");
+
+	int size = 5;
+		int adjacency_array[5][5] = {
+									{0,1,1,1,1},
+									{1,0,1,1,1},
+									{1,1,0,1,1},
+									{1,1,1,0,1},
+									{1,1,1,1,0}
+		};
+
+		matrix adjacency(size, vector<int>(size));
+		for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 5; j++){
+				adjacency[i][j] = adjacency_array[i][j];
+			}
+		}
+
+
+		State *state1 = new State(&adjacency,0);
+
+
+	//State *state1 = reader.getFirstStateFromFile("zk.txt");
 	cout << "state1->isBipartite() = " << state1->isBipartite() << endl;
 	cout << "state1->getNumberOfEdges() = " << state1->getNumberOfEdges() << endl;
 	stack<State*> state_stack;
