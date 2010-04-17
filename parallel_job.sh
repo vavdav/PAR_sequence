f9c964ffe57f3dbe0d027157d5ac6b40d66fc65d
Petr Smejkal <xmejkal@gmail.com>
2010-04-17 17:44:19 +0200

    jednoduchy skript

--- parallel_job.sh
+++ parallel_job.sh
@@ -1,9 +1,2 @@
-INFINIBAND="true"
 MY_PARALLEL_PROGRAM="./par graph5.txt"
-if 
-[[ ${INFINIBAND} = "true" ]]
-then
 mpirun -np $NSLOTS ${MY_PARALLEL_PROGRAM}
-else
-mpirun --mca btl tcp,self -np $NSLOTS ${MY_PARALLEL_PROGRAM}
-fi
