d8b0b4c718dc5344ddb64dc792c4984e81088583
Petr Smejkal <xmejkal@gmail.com>
2010-04-17 20:59:56 +0200

    odesilani matice

--- Bipartite.cpp
+++ Bipartite.cpp
@@ -35,8 +35,11 @@ void logMeIntoCout(int rank, int numProcesses){
 
 int main (int argc, char *argv[] )
 {
+	stack<State*> state_stack;
+	State *state1;
+	char *buffer;
+
 	Communicator communicator(argc, argv);
-	//logMeIntoFile(communicator.rank, communicator.numProcesses);
 	logMeIntoCout(communicator.rank, communicator.numProcesses);
 
 /*
@@ -44,60 +47,48 @@ int main (int argc, char *argv[] )
 	int message_length = message.length() + 1;
 	char* buffer = new char[message_length];
 	strcpy(buffer, message.c_str());
-
+*/
 
 	if(communicator.rank == 0){
+
+		char *fileName;
+		if ( argc != 2 ) {
+			cout<<"usage: "<< "PAR" <<" <filename>" << endl;
+			return -1;
+		}
+		fileName = argv[1];
+		//fileName = "graph10.txt";
+		GraphReader reader;
+		state1 = reader.getFirstStateFromFile(fileName);
+		state_stack.push(state1);
+
+		int length = state1->numberOfVertices*state1->numberOfVertices + sizeof(int)*2;
+		buffer = new char[length];
+		state1->serialize(buffer, 0);
 
 		int tag = 1;
 		//char[MESSAGE_SIZE] buffer;
 		for(int receiver = 1; receiver < communicator.numProcesses; receiver++){
-			cout << "Processor " << communicator.rank << ": Sending [" << buffer << "] to processor " << receiver << endl;
-			MPI_Send (buffer, message_length, MPI_CHAR, receiver, tag, MPI_COMM_WORLD);
-			cout << "Processor " << communicator.rank << ": message sent to processor " << receiver << endl;
+			cout << "Processor " << communicator.rank << ": Sending " << endl;
+			//state1->print();
+			MPI_Send (&state1->numberOfVertices, 1, MPI_INT, receiver, tag, MPI_COMM_WORLD);
+			MPI_Send (buffer, length, MPI_PACKED, receiver, tag, MPI_COMM_WORLD);
 		}
+		delete state1;
 	}
 	else
 	{
 		MPI_Status status;
-		int length;
-		MPI_Recv(buffer, message_length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		      /* getting info about received message */
-		      //MPI_Get_count(&status, MPI_CHAR, &length);
-		      /* message *//*
-		      cout << "Processor " << communicator.rank << ": Received message: [" << buffer << "]" << endl;
+		int numVertices;
+		MPI_Recv(&numVertices, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		int length = numVertices*numVertices + sizeof(int)*2;
+		MPI_Recv(buffer, length, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		State *deserializedState = State::deserialize(buffer, 0, state1->numberOfVertices);
+		cout << "Processor " << communicator.rank << ": Received: " << endl;
+		if(communicator.rank == 1) deserializedState->print();
+		delete deserializedState;
 	}
-
-	cout << "Processor " << communicator.rank << " ending" << endl;
 	delete buffer;
-
-
-	communicator.finalize();
-	return 0;
-*/
-
-	char *fileName;
-
-
-	if ( argc != 2 ) {
-		cout<<"usage: "<< "PAR" <<" <filename>" << endl;
-		return -1;
-	}
-	fileName = argv[1];
-	//fileName = "graph10.txt";
-	GraphReader reader;
-	State *state1 = reader.getFirstStateFromFile(fileName);
-	stack<State*> state_stack;
-	state_stack.push(state1);
-
-	int length = state1->numberOfVertices*state1->numberOfVertices + sizeof(int)*2;
-	char *buffer = new char[length];
-
-	state1->serialize(buffer, 0);
-
-	State *deserializedState = State::deserialize(buffer, 0, state1->numberOfVertices);
-
-	deserializedState->print();
-
 	communicator.finalize();
 	return 0;
 
